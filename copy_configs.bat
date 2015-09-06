fe20b476dbcd8fe615bf54f22988cc33ed7ac614
BubblesToTheLimit <mond.beton@googlemail.com>
2015-09-06 18:10:26 +0200

    First Adjustment to the big config

--- copy_configs.bat
+++
@@ -1,7 +0,0 @@
-xcopy C:\Users\FBrilej\AppData\Roaming\.emacs C:\Users\FBrilej\Desktop\Projekte\emacs-config\ /y
-
-xcopy C:\Users\FBrilej\AppData\Roaming\.emacs.d\my-packages.el C:\Users\FBrilej\Desktop\Projekte\emacs-config\ /y
-
-xcopy C:\Users\FBrilej\AppData\Roaming\.emacs.d\my-loadpackages.el C:\Users\FBrilej\Desktop\Projekte\emacs-config\ /y
-
-xcopy C:\Users\FBrilej\AppData\Roaming\.emacs.d\work.el C:\Users\FBrilej\Desktop\Projekte\emacs-config /y
