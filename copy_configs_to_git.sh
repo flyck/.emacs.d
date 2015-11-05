c428852e6bb6e38b15474bd99b5f0acbca9d60de
BubblesToTheLimit <mond.beton@googlemail.com>
2015-11-05 12:36:23 +0100

    deleted old stuff

--- copy_configs_to_git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp -rf ~/.emacs /home/fbrilej/Documents/.emacs.d/
-cp -rf ~/.emacs.d/my-packages.el /home/fbrilej/Documents/.emacs.d/
-cp -rf ~/.emacs.d/my-loadpackages.el /home/fbrilej/Documents/.emacs.d/
-cp -rf ~/.emacs.d/work.el /home/fbrilej/Documents/.emacs.d/
-cp -rf ~/.emacs.d/fakecygpty.el /home/fbrilej/Documents/.emacs.d/
-echo "configs of the current user copied to git directory"
--- replace_current_config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp -rf .emacs ~/
-cp -rf my-packages.el ~/.emacs.d/
-cp -rf my-loadpackages.el ~/.emacs.d/
-cp -rf work.el ~/.emacs.d/
-echo "configs copied to home directory of current user"
