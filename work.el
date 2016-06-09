05773aa3ce17256eeb0b0b00142b1dcfcbc8fdf0
BubblesToTheLimit <mond.beton@googlemail.com>
2016-06-09 15:20:49 +0200

    some makeup and resorting

--- work.el
+++ work.el
@@ -1,3 +1,4 @@
+;; Proxy settings
 (setq url-proxy-services '(("no_proxy" . "work\\.com")
                            ("http" . "172.16.8.250:3128")
 			   ("https" . "172.16.8.250:3128")))
@@ -10,13 +11,18 @@
         ("CANCELED" . (:foreground "#b5bd68"
 		       ;;:strike-through t
 				   :weight bold))))
+
 ;; Set the agenda-files on my working laptop (windows) and possible linux VMs respectively
 (if (eq system-type 'windows-nt)
     (setq org-agenda-files (list "C:\\Users\\FBrilej\\Desktop\\Projekte\\org\\projects.org"
-				 "C:\\Users\\FBrilej\\Desktop\\Projekte\\request-tracker\\ticketsystem.org")))
-(if (eq system-type 'gnu/linux)
-    (setq org-agenda-files (list "~/Documents/org/projects.org"
-				 "~/Documents/request-tracker/ticketsystem.org")))
+				 "C:\\Users\\FBrilej\\Desktop\\Projekte\\request-tracker\\ticketsystem.org"))
+    ;; Open my projects file which contains all of my scheduling
+    (switch-to-buffer (find-file-noselect "C:\\Users\\FBrilej\\Desktop\\Projekte\\org\\projects.org"))
+  )
+;; (if (eq system-type 'gnu/linux)
+;;     (setq org-agenda-files (list "~/Documents/org/projects.org"
+;; 				 "~/Documents/request-tracker/ticketsystem.org")))
+
 ;; org-capture setup
 (setq org-capture-templates
       '(("a" "My TODO task format." entry
@@ -24,9 +30,6 @@
          "* TODO %?
 SCHEDULED: %t")))
 (setq org-refile-targets '((org-agenda-files . (:maxlevel . 2))))
-
-;; Open my projects file which contains all of my scheduling
-(switch-to-buffer (find-file-noselect "C:\\Users\\FBrilej\\Desktop\\Projekte\\org\\projects.org"))
 
 ;; Coding System
 ;; I use the unix coding system everywhere
@@ -43,13 +46,3 @@ SCHEDULED: %t")))
 (if (window-system)
   (set-frame-position (selected-frame) 0 0)
   (set-frame-height (selected-frame) 120))
-;; Experimental stuff below:
-;; Maximize the screen on Windows-Systems
-;; (add-hook 'after-init-hook '(lambda () (w32-send-sys-command #xf030)))
-;; (w32-send-sys-command #xf030)
-;; (defun w32-maximize-frame ()
-;;   "Maximize the current frame"
-;;   (interactive)
-;;   (w32-send-sys-command 61488)
-;; )
-;; (w32-maximize-frame)
