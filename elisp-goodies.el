b5ec2ad103e36359b5b7eceb4f19c3cc72c1bd28
BubblesToTheLimit <mond.beton@googlemail.com>
2016-07-08 18:42:36 +0200

    added smileys and documentation

--- elisp-goodies.el
+++ elisp-goodies.el
@@ -1,9 +1,13 @@
 ;; tea-timer (after X minutes, no repeat)
 (defun fbr/tea-timer(minutes)
   "my function for timing tee"
+  (interactive "nHow many minutes does the tea need? ")
   ;; setup
   (require 'alert)
   (setq alert-default-style 'libnotify)
   ;; actual command
-  (run-with-timer (* minutes 60) nil 'alert "Tea is ready :)" :title "Tea-time")
+  ;; smiley-code: \xF0\x9F\x98\x84
+  ;; hourglass-code: \xE2\x8C\x9B
+  ;; Listing: http://apps.timwhitlock.info/emoji/tables/unicode
+  (run-with-timer (* minutes 60) nil 'alert "Tea is ready \xF0\x9F\x98\x84" :title "Tea-time")
   )
