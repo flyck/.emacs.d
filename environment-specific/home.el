861b8f3213660479d024813a7d3e098744094d1a
BubblesToTheLimit <mond.beton@googlemail.com>
2016-08-08 18:01:46 +0200

    open the file after changing org-mode settings!

--- environment-specific/home.el
+++ environment-specific/home.el
@@ -2,7 +2,6 @@
                         (concat "C:/Users/" (getenv "USERNAME") "/Dropbox/org/gtd/tasks.org")
                         (concat "C:/Users/" (getenv "USERNAME") "/Dropbox/org/hobby/dactyl-keyboard-guide/index.org")
                         ))
-(switch-to-buffer (find-file-noselect (concat "C:/Users/" (getenv "USERNAME") "/Dropbox/org/gtd/tasks.org")))
 
 ;; Coding System
 (prefer-coding-system 'utf-8-dos)
@@ -16,6 +15,8 @@
 (setq org-todo-keyword-faces
       '(("TODO" . org-warning) ("PENDING" . "#f0c674")
         ("CANCELED" . (:foreground "#b5bd68" :weight bold))))
+
+(switch-to-buffer (find-file-noselect (concat "C:/Users/" (getenv "USERNAME") "/Dropbox/org/gtd/tasks.org")))
 
 ;; load my manually installed yasnippet package
 (add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
