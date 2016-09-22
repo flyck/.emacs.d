6d551497f045e89e2e8d14cbb629c1f62e9ef936
BubblesToTheLimit <mond.beton@googlemail.com>
2016-09-22 16:48:51 +0200

    added tests about redoing the orgzly formatting

--- elisp-goodies.el
+++ elisp-goodies.el
@@ -27,3 +27,28 @@
 ;; (shell-command-to-string "echo ")
 ;; (start-process "mvn-exec" "~\.emacs.d\notifu\notifu.exe" "/m" "aads" "/d" "10000" )
 ;; (setq debug-on-error t)
+
+
+;; Correct orglzy formatting
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/index.html#SEC_Contents
+(defun fbr/my-orgzly-reformatting ()
+  "Reformat the file to undo changes from orgzly"
+  (interactive)
+  (setq m (mark-marker))
+  (beginning-of-buffer)
+  (while (re-search-forward "^\*.*" nil t)
+    (next-line)
+    (if (looking-at "^$")
+        ;; delete the current line if it's empty without sending it to kill-ring
+        (let ((beg (progn (forward-line 0)
+                          (point))))
+          (forward-line 1)
+          (delete-region beg (point)))
+      nil ;; else part does nothing
+      )
+    )
+  (set-mark-command m)
+  )
+;; The idea of an external script:
+;; http://emacs.stackexchange.com/questions/12148/how-to-pretty-format-code-auto-insert-newlines-indent-etc
+;; das multiple-line flag aus perl nutzen, einfach ein perl script über die aktuelle datei laufen lassen
