93fa8db976eedfa5f7c3f04bd1097878e34282d4
flyck <felix.brilej@googlemail.com>
2023-05-01 15:22:24 +0200

    Update bind-key

--- elpa/bind-key-20191110.416/bind-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "bind-key" "20191110.416" "A simple way to manage personal keybindings" 'nil :commit "2b8f8a3f156e61ae6d8c7b7ce1deea3f0f6e1599" :keywords '("keys" "keybinding" "config" "dotemacs") :authors '(("John Wiegley" . "johnw@newartisans.com")) :maintainer '("John Wiegley" . "johnw@newartisans.com") :url "https://github.com/jwiegley/use-package")
