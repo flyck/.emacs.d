222f284269e3f20821cd6ecb2134c8300dc4f829
flyck <felix.brilej@googlemail.com>
2025-08-20 17:46:33 +0200

    chore: remove old, unused notifu stuff for windows

--- elisp-goodies.el
+++ elisp-goodies.el
@@ -1,7 +1,7 @@
 ;; tea-timer (after X minutes, no repeat)
 (defun fbr/tea-timer(minutes)
   "my function for timing tee"
-  (interactive "nHow many minutes does the tea need? ")
+  (interactive "How many minutes does the tea need? ")
   ;; setup
   (require 'alert)
   (setq alert-default-style 'libnotify)
@@ -17,23 +17,6 @@
   (yas-recompile-all)
   (yas-reload-all)
   )
-
-;; (shell-command "Msg asd")
-;; (call-process "~\\.emacs.d\\notifu\\notifu.exe" nil t nil "/m" "aads" "/d" "10000" "/i" "~\\emacs.d\\emacs.ico")
-;; ;; ich brauche start-process
-;; (start-process "my-process" "~\\.emacs.d\\notifu\\notifu.exe" "/m" "aads" "/d" "10000" "/i" "~\\emacs.d\\emacs.ico")
-;; (start-process "my-process" "~/.emacs.d/notifu/notifu.exe" "/m" "aads" "/d" "10000")
-
-;; (defun execvp (&rest args)
-;;   "Simulate C's execvp() function.
-;; Quote each argument seperately, join with spaces and call shell-command-to-string to run in a shell."
-;;   (let ((cmd (mapconcat 'shell-quote-argument args " ")))
-;;     (shell-command-to-string cmd)))
-
-;; (shell-command-to-string "echo ")
-;; (start-process "mvn-exec" "~\.emacs.d\notifu\notifu.exe" "/m" "aads" "/d" "10000" )
-;; (setq debug-on-error t)
-
 
 ;; Correct orglzy formatting
 ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/index.html#SEC_Contents
